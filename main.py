8ab7def4e9974b0591821b8c93787f5d2977fca4
powerm17 <105803016+powerm17@users.noreply.github.com>
2024-06-17 12:49:20 -0400

    Create main.py
    Accessing placeholder values in list

---
+++ main.py
@@ -0,0 +1,7 @@
+#The number of houses in the neighborhoods is unknown 
+list1 = []
+
+list1 = ['physics', 'chemistry', 1997, 2000]
+list2 = [1, 2, 3, 4, 5, 6, 7 ]
+print ("list1[0]: ", list1[0])
+print ("list2[1:7]: ", list2[1:7])
